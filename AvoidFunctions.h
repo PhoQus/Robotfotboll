4a26284edc668adaaca965b9e175d79e85368219
PhoQus <phoquslgen@gmail.com>
2017-05-10 10:28:29 +0200

    Added a test function that return true when robotA should avoid.

--- AvoidFunctions.h
+++ AvoidFunctions.h
@@ -1,17 +1,3 @@
-  //int32_t pulses = 1920; // pulses per revolution
-  //double circumference = 30.9211; // the wheels circumference
-  int32_t rightpos; 
-  int32_t leftpos;
-  int32_t tmprightpos;
-  int32_t tmpleftpos;
-  int32_t startTurnRightPos;
-  int32_t startTurnLeftPos;
-  //int32_t encoderVinkel1 = 350;
-  int32_t runDistance = 400;
-  double degreeToEncoder=21.67;
-  double timeLastLoop = 0;
-  double cmToEncoder = (1920/30.9211);
-
 
   bool CalculationsDoneAvoid(){
     return true;
@@ -25,6 +11,31 @@
     return false;
     }
 
+  bool ShouldAvoid(){
+    if(RobotAx <10){
+    return true;
+    }
+    if(RobotAx >150){
+    return true;
+    }
+    if(RobotAy <10){
+    return true;
+    }
+    if(RobotAy >150){
+    return true;
+    }
+    
+    if(abs(RobotAx - RobotBx)<30){
+      return true;
+    }
+    
+    
+    if(abs(RobotAy - RobotBy)<30){
+      return true;
+    }
+    
+    return false;
+  }
     
   void CalculateAvoid(){
       planState = wait;
